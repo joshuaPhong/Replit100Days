5eb8535cb0c35dc302f4af0dc0c5d6acfd4b7a83
joshuaPhong <klx2044@yahoo.com>
2023-05-16 10:56:45 +1200

    day 2

---
+++ day3.py
@@ -0,0 +1,9 @@
+# concatenating strings and variables
+print("ENTER THE FOLLOWING")
+cooking = input("A method of cooking: ")
+food = input("A type of food: ")
+burned = input("Describe burned food: ")
+plant = input("A type of plant: ")
+item = input("A household item: ")
+
+print("You will be", cooking, food, "with", burned, plant, "on a bed of", item)
