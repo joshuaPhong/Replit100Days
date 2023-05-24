8ae8ffc4709357b681fadc53280fb5fc24d66ba6
joshuaPhong <klx2044@yahoo.com>
2023-05-24 18:03:49 +1200

    day nine challenge. casting boolean comparison

--- Day5Ifthen/day9CastingNumbers.py
+++ Day5Ifthen/day9CastingNumbers.py
@@ -1,11 +1,25 @@
-myScore = int(input("Your score: "))
-if myScore > 100000:
-  print("Winner!")
+# myScore = int(input("Your score: "))
+# if myScore > 100000:
+#     print("Winner!")
+# else:
+#     print("Try again 😭")
+#
+# myPi = float(input("What is Pi to 3dp? "))
+# if myPi == 3.142:
+#     print("Exactly!")
+# else:
+#     print("Try again 😭")
+#  challenge
+generation = int(input("What year were you born >>> "))
+if 1925 < generation <= 1946:
+    print("You are a traditionalist 👴")
+elif 1947 <= generation <= 1964:
+    print("You are a baby boomer 👶")
+elif 1965 <= generation <= 1981:
+    print("You are a gen X 😎")
+elif 1982 <= generation <= 1995:
+    print("You are a millennial 🐸")
+elif 1996 <= generation <= 2015:
+    print("You are a gen Y 🥱")
 else:
-  print("Try again 😭")
-
-myPi = float(input("What is Pi to 3dp? "))
-if myPi == 3.142 :
-  print("Exactly!")
-else:
-  print("Try again 😭")
+    print("You have no generation")
