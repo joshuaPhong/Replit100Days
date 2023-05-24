44ef563aac2eca3080666b325c9846cfc5e13477
joshuaPhong <klx2044@yahoo.com>
2023-05-24 16:42:54 +1200

    casting, int() float()

---
+++ Day5Ifthen/day9CastingNumbers.py
@@ -0,0 +1,11 @@
+myScore = int(input("Your score: "))
+if myScore > 100000:
+  print("Winner!")
+else:
+  print("Try again 😭")
+
+myPi = float(input("What is Pi to 3dp? "))
+if myPi == 3.142 :
+  print("Exactly!")
+else:
+  print("Try again 😭")
