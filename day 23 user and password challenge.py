e15e8c11dba9d258db10f93c1dda4f545ab2728c
joshuaPhong <klx2044@yahoo.com>
2023-05-29 13:58:35 +1200

    day 23 password challenge

--- day 23 user and password challenge.py
+++ day 23 user and password challenge.py
@@ -1,13 +1,19 @@
+# a login function.
+# loops through user input until the correct credentials are entered
 def login():
     while True:
         user = input("What is your username >>>")
         word = input("What is your password >>>")
         if user == "user" and word == "word":
-            print("You in....")
+            print("Your in....")
             break
         else:
             print("Incorrect")
             continue
 
 
+print("LOGIN")
+print("-" * 6)
+print("Username = user: Password = word")
+# call the login function
 login()
