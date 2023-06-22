e02d4f95ef1d47294c44c07dccab2328c6ed83bb
joshuaPhong <klx2044@yahoo.com>
2023-06-23 02:25:15 +1200

    day 53 inventory project auto_save() add_item() debug_mode inventory.txt

--- day_53_game_inventory.py
+++ day_53_game_inventory.py
@@ -8,9 +8,28 @@
 # Removing an item deletes it from the file.
 # View is trickier. It should output the name of the item and tell you how many of those items you have.
 # Use auto-save and auto-load with try... except.
-import time, os
+
+# IMPORTS
+import os
+import time
+import traceback
+
+# VARIABLES
+inventory = []
+debug_mode = True
 
 
+def auto_save(file):
+    try:
+        with open(file, "w") as f:
+            f.write(str(inventory))
+    except Exception as err:
+        print(f"Save error: {err}")
+        if debug_mode:
+            print(traceback.format_exc())
+
+
+# clears the screen and pauses the program
 def tidy_screen():
     print()
     os.system("cls" if os.name == "nt" else "clear")
@@ -18,7 +37,11 @@ def tidy_screen():
 
 
 def add_item():
-    pass
+    print("Add Items to Your Inventory")
+    item = input("Enter an item: ")
+    inventory.append(item)
+    auto_save("inventory.txt")
+    print(inventory)
 
 
 def view_items():
@@ -29,15 +52,22 @@ def remove_item():
     pass
 
 
+# a main menu message
 def menu_message():
     print("Your Inventory ⚔️🗡️🔪🪄🍊🔪\nUse it well and conquer the foe.")
 
 
+# the main menu
 def menu():
+    # clear the screen
     tidy_screen()
+    # output the menu message
     menu_message()
+    # start a loop for menu options
     while True:
+        # menu options
         menu_choice = input("1. Add item\n2. View items\n3. Remove items\nEnter >>> ")
+        # try the cast input and call the appropriate function
         try:
             if int(menu_choice) == 1:
                 add_item()
@@ -48,6 +78,7 @@ def menu():
             else:
                 print("Input error, try again with a number 1-3")
                 continue
+        #         if the input fails, display a message clear the screen and send the user back to the menu options
         except Exception as err:
             print(f"Error {err}\nTry again")
             time.sleep(1)
@@ -55,4 +86,5 @@ def menu():
             continue
 
 
+# call the menu to start the program
 menu()
