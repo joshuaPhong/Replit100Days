902f19f76229d081ceddceb74d6940d228a883f0
joshuaPhong <klx2044@yahoo.com>
2023-07-24 19:32:02 +1200

    a tkinter window

---
+++ day_66_tkinter.py
@@ -0,0 +1,11 @@
+# import the tkinter module
+import tkinter as tk
+# create the window (gui)
+window = tk.Tk()
+# add a title tyo the window
+window.title("Joshua's first window")
+# size the window
+window.geometry("400x400")
+
+# run the tkinter window
+tk.mainloop()
